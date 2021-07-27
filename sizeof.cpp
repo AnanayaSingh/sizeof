68f75d2130dcc0c53c35bedcaa3c2b74a29ed359
Ananya16coder <84061632+Ananya16coder@users.noreply.github.com>
2021-07-27 11:21:57 +0530

    create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    char c;
+    bool d;
+    double e;
+    cout<<"size of int "<<sizeof(a)<<endl;
+    cout<<"size of float "<<sizeof(b)<<endl;
+    cout<<"size of character "<<sizeof(c)<<endl;
+    
+    cout<<"size of boolean "<<sizeof(d)<<endl;
+    cout<<"size of double "<<sizeof(e)<<endl;
+    short int si;
+    long int li;
+    cout<<"size of short int "<<sizeof(si)<<endl;
+    cout<<"size of long int "<<sizeof(li)<<endl;
+
+
+    return 0;
+}
